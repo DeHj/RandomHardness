e134863c5195531d373ad86906f853fbd9690baa
Denis <denisabsdenis@yandex.ru>
2020-12-30 02:02:41 +0300

    Clean unnecessary comments

--- src/main/java/randomhardness/DedicatedServerProxy.java
+++ src/main/java/randomhardness/DedicatedServerProxy.java
@@ -3,40 +3,18 @@ package randomhardness;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 
-/**
- * DedicatedServerProxy is used to set up the mod and start it running on dedicated servers.  It contains all the code that should run on the
- *   dedicated servers.  This is almost never required.
- *   For more background information see here http://greyminecraftcoder.blogspot.com/2013/11/how-forge-starts-up-your-code.html
- */
 public class DedicatedServerProxy extends CommonProxy
 {
-
-    /**
-     * Run before anything else. Read your config, create blocks, items, etc, and register them with the GameRegistry
-     */
     public void preInit() {
         super.preInit();
-
-        //randomhardness.custom_block_stone.StartupCommon.preInitCommon();
     }
 
-    /**
-     * Do your mod setup. Build whatever data structures you care about. Register recipes,
-     * send FMLInterModComms messages to other mods.
-     */
     public void init() {
         super.init();
-
-        //randomhardness.custom_block_stone.StartupCommon.initCommon();
     }
 
-    /**
-     * Handle interaction with other mods, complete your setup based on this.
-     */
     public void postInit() {
         super.postInit();
-
-        //randomhardness.custom_block_stone.StartupCommon.postInitCommon();
     }
 
     @Override
