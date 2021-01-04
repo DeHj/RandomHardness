b0804ffb185e2ada596cb426a6d1ff088c166f15
Denis <denisabsdenis@yandex.ru>
2021-01-04 20:09:25 +0300

    Result of TNT recipe increased twice

--- src/main/java/randomhardness/gunpowder_mining/StartupCommon.java
+++ src/main/java/randomhardness/gunpowder_mining/StartupCommon.java
@@ -1,5 +1,6 @@
 package randomhardness.gunpowder_mining;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -7,18 +8,32 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import net.minecraftforge.registries.GameData;
 import randomhardness.RandomHardness;
 import randomhardness.config.ConfigAgent;
 
 public class StartupCommon {
     public static void preInitCommon()
     {
+        ResourceLocation optionalGroup = new ResourceLocation("");
+
         GameRegistry.addShapelessRecipe(
-                new ResourceLocation(RandomHardness.MODID, "gunpowder"),
-                new ResourceLocation(""),
+                new ResourceLocation(RandomHardness.MODID, "gunpowder"), optionalGroup,
                 new ItemStack(Items.GUNPOWDER, 2),
                 CraftingHelper.getIngredient(Items.COAL),
                 CraftingHelper.getIngredient(randomhardness.sulfur.StartupCommon.ITEM_SULFUR));
+
+
+        GameRegistry.addShapedRecipe(
+                new ResourceLocation("minecraft", "tnt"), optionalGroup,
+                new ItemStack(Blocks.TNT, 2),
+                new Object[]{
+                        "X#X",
+                        "#X#",
+                        "X#X",
+                        'X', Items.GUNPOWDER,
+                        '#', Blocks.SAND
+                });
     }
 
     public static void initCommon()
