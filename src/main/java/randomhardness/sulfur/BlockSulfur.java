682b954e84d8dadde992e13f2c72f61163ecc129
Denis <denisabsdenis@yandex.ru>
2021-01-08 04:30:38 +0300

    Fix sulfur block constructor
    Now there is sulfur block in creative tab.

--- src/main/java/randomhardness/sulfur/BlockSulfur.java
+++ src/main/java/randomhardness/sulfur/BlockSulfur.java
@@ -3,10 +3,19 @@ package randomhardness.sulfur;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockSulfur extends Block {
-    public BlockSulfur() {
-        super(Material.ROCK);
+    public BlockSulfur(Material material) {
+        super(material);
         this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.SOLID;
     }
 }
